60637e214f23409ac3960a35f885c8b900ea517a
ZWELI8 <zwelibanzin423@gmail.com>
2023-03-23 21:28:37 +0200

    Update Two_Sum.py
    Final two sum code

--- Two_Sum.py
+++ Two_Sum.py
@@ -6,12 +6,9 @@ class Solution:
         for i in range(len(self.nums)):
             for j in range(1,len(self.nums)):
                 if(self.nums[i]+self.nums[j] == target and i!=j):
-                    print("["+str(i)+","+str(j)+"]")
-                    p+=1
-                    break
-            if p!=0:
-                break
+                    return i,j
+
 
 p=Solution()
 x= [2,5,5,11]
-p.twoSum(x,10)
+print(p.twoSum(x,10))
